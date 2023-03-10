5b01cd897959fe35c265a6e059141ece6a566717
pluxurypinkphloydrockstar <theDianaB15@gmail.com>
2023-03-11 01:04:28 +0300

    добавлены методы read_from_txt и write_to_txt

--- LR2/main.rb
+++ LR2/main.rb
@@ -1,22 +1,39 @@
 require_relative 'student'
+require_relative 'student_short'
 
-student1 = Student.new('Баев', 'Андрей', 'Николаевич')
-student2 = Student.new('Баева', 'Диана', 'Николаевна', { id: 1, telegram: '@pluxurypinkphloydrockstar' })
-student3 = Student.new('Иванов', 'Алексей', 'Алексеевич', { phone: '79259511100', email: 'ivanov_alexey_2000@mail.ru', git: '@alexiv' })
+def read_from_txt(file_path)
+  raise ArgumentError, 'File not found' unless File.exist?(file_path)
+
+  stud_file = File.open(file_path, 'r')
+  result = ''
+  stud_file.each do |line|
+    result << line
+  end
+  stud_file.close
+  students_list = []
+  stud_list = JSON.parse(result)
+
+  stud_list['StudentList'].each do |obj|
+    students_list << Student.init_from_json(obj.to_json)
+  end
+  students_list
+end
+
+def write_to_txt(file_path, student_list)
+  result = '{"StudentList":['
+  student_list.each do |student|
+    result += student.to_json + ","
+  end
+  result = result.chop + "]}"
+  File.write(file_path, result)
+end
+
+student1 = Student.new('Баева', 'Диана', 'Николаевна')
+student2 = Student.new('Баев', 'Андрей', 'Николаевич',  id: 1, telegram: '@baev_andrey' )
+student3 = Student.new('Иванов', 'Алексей', 'Алексеевич',  phone: '79181461800', email: 'alexiv@gmail.com', git: '@alexiv' )
 
 
-puts student1
-puts student2
-puts student3
+student_list = [student1, student2, student3]
 
-
-puts Student.valid_phone?('79259511100')
-puts Student.valid_phone?('+9 1323-122-33-78')
-
-puts Student.valid_acc?('@alexiv')
-puts Student.valid_email?('baevadianawrk@gmail.com')
-
-puts student3.git?
-
-student1.set_contacts({telegram: '@user1995'})
-puts student1
+puts read_from_txt('/home/diana/students_list.txt')
+write_to_txt('/home/diana/new_students_list.txt', student_list)
