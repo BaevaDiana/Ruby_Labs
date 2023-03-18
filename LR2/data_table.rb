064d6e8ea5038f4b77052119cab47ae60a3a1a73
pluxurypinkphloydrockstar <theDianaB15@gmail.com>
2023-03-18 09:39:46 +0300

    очень большой перекрой кода

---
+++ LR2/data_table.rb
@@ -0,0 +1,33 @@
+class DataTable
+
+  attr_reader :rows_count, :cols_count
+
+  # конструктор 
+  def initialize(table)
+    self.rows_count = table.length
+    max_cols = 0
+    table.each do |row|
+      max_cols = row.length if row.size > max_cols
+    end
+    self.cols_count = max_cols
+    self.table = table
+  end
+
+  # метод, воз-ий элемент по номеру строки и столбца
+  def get_elements(row, col)
+    return nil if row >= rows_count || row.negative?
+    return nil if col >= cols_count || col.negative?
+
+    table[row][col]
+  end
+
+  def to_s
+    table.map { |row| "[#{row.join(', ')}]" }.join("\n")
+  end
+
+  private
+
+  attr_accessor :table
+  attr_writer :rows_count, :cols_count
+
+end
