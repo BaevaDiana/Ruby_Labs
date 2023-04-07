1fee03ab333a64175a5b61a1bf5686dd3e146cf7
pluxurypinkphloydrockstar <theDianaB15@gmail.com>
2023-04-08 00:01:27 +0300

    добавлены комментарии в student_list_db

--- LR3/repositories/student_list_db.rb
+++ LR3/repositories/student_list_db.rb
@@ -2,10 +2,12 @@ require 'mysql2'
 require_relative '/home/diana/LR3/models/student'
 
 class StudentListDB
+  # стандартный конструктор
   def initialize(db_config)
     self.client = Mysql2::Client.new(db_config)
   end
 
+  # поиск студента по id
   def student_by_id(student_id)
     client.query("SELECT * FROM students WHERE id = #{student_id}").first.tap do |result|
       return Student.from_hash(result) if result
@@ -13,21 +15,25 @@ class StudentListDB
     nil
   end
 
+  # добавление студента
   def add_student(student)
     stmt = client.prepare('INSERT INTO students (first_name, last_name, surname, phone, telegram, mail, git) VALUES (?, ?, ?, ?, ?, ?, ?)')
     stmt.execute(*student_fields(student))
     client.last_id
   end
 
+  # удаление студента
   def remove_student(student_id)
     client.query("DELETE FROM students WHERE id = #{student_id}")
   end
 
+  # обновление студента
   def replace_student(student_id, student)
     stmt = client.prepare('UPDATE students SET first_name = ?, last_name = ?, surname = ?, phone = ?, telegram = ?, mail = ?, git = ? WHERE id = ?')
     stmt.execute(*student_fields(student), student_id)
   end
 
+  # количество студентов
   def student_count
     client.query('SELECT COUNT(id) FROM students').first.values.first
   end
